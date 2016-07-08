6567502ef2d2cdf52ab598db2e28cc2b8f92ca07
Jan Vermeir <jvermeir@xebia.com>
2016-07-08 21:51:32 +0200

    Added empty xml files in src directory and fixed path to ext directory in run.sh

--- xld/run.sh
+++ xld/run.sh
@@ -3,8 +3,10 @@
 # Run XLDeploy in Docker. Mount some volumes to local directories so we can easily change files as needed.
 # Usage: ./run.sh
 
+srcDirName="$(dirname "$PWD")/src"
+
 docker run -it --rm \
    --name xldeploy \
    -p 4516:4516 \
-   -v src:/lib/xl-deploy-5.5.1-server/ext \
-   xldeploy 
+   -v ${srcDirName}:/lib/xl-deploy-5.5.1-server/ext \
+   xldeploy
